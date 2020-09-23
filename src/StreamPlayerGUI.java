0fda2d27c9a473d4a44d8067bf78343e3216362e
Cory Reardon <cjreardon@gmail.com>
2020-09-22 18:29:14 -0700

    Changed the player to have a table and one button.

--- src/StreamPlayerGUI.java
+++ src/StreamPlayerGUI.java
@@ -2,9 +2,9 @@ import javazoom.jlgui.basicplayer.BasicPlayer;
 import javazoom.jlgui.basicplayer.BasicPlayerException;
 
 import javax.swing.*;
+import javax.swing.table.TableColumn;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
@@ -14,34 +14,58 @@ public class StreamPlayerGUI extends JFrame {
     BasicPlayer player;
 
     JPanel main;
-    JButton rock, the80s, purDeutsch;//the three buttons
-    JLabel  nowPlaying;
+    JButton playButton;
+    JTable table;
+    JScrollPane scrollPane;
+    int currentSelectedRow;
     ButtonListener bl;
     public StreamPlayerGUI() {
         player = new BasicPlayer();
-
         main = new JPanel();
-        main.setLayout(new FlowLayout());
 
+        //Creates the panel with a vertical layout.
+        main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
+
+        //Creates a Button Listener to attach to the playButton.
         bl = new ButtonListener();
 
-        rock = new JButton("Rock");
-        rock.addActionListener(bl);
+        //Sets the button's text, attaches the listener, and also sets it's size and alignment.
+        playButton = new JButton("Play");
+        playButton.addActionListener(bl);
+        playButton.setMinimumSize(new Dimension(450, 25));
+        playButton.setMaximumSize(new Dimension(450, 25));
+        playButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        the80s = new JButton("The 80's");
-        the80s.addActionListener(bl);
+        //The set of data that will be in the table.
+        String[] columns = {"Station URL", "Description"};
+        String[][] data = {{"https://mp3.ffh.de/ffhchannels/hqrock.mp3", "Rock"},
+                            {"https://mp3.ffh.de/ffhchannels/hq80er.mp3", "The 80's"},
+                            {"https://mp3.ffh.de/ffhchannels/hqdeutsch.mp3", "Per Deutsch"},
+                            {"https://mp3.ffh.de/ffhchannels/hq90er.mpr", "The 90's"}};
 
-        purDeutsch = new JButton("Pur Deutsch");
-        purDeutsch.addActionListener(bl);
+        table = new JTable(data, columns);
 
+        //Creates a new listener for the mouse attached to the table.
+        MouseListener mouseListener = new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                currentSelectedRow = table.getSelectedRow();
+                System.out.println("Selected index " + currentSelectedRow);
+            }
+        };
 
-        nowPlaying = new JLabel("Now playing: nothing");
+        //Attaches the mouseListener to the table.
+        table.addMouseListener(mouseListener);
 
-        main.add(rock);
-        main.add(the80s);
-        main.add(purDeutsch);
-        main.add(nowPlaying);
-        this.setTitle("StreamPlayer by Dr. Hoffman");//change the name to yours
+        //Sets the width of the URL column to be larger.
+        TableColumn column = table.getColumnModel().getColumn(0);
+        column.setPreferredWidth(250);
+
+        scrollPane = new JScrollPane(table);
+
+        //Adds all the components to the panel.
+        main.add(scrollPane);
+        main.add(playButton);
+        this.setTitle("StreamPlayer by Cory Reardon");//change the name to yours
         this.setSize(400, 150);
         this.add(main);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -51,23 +75,16 @@ public class StreamPlayerGUI extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            String url=null;
-            if("Rock".equals(e.getActionCommand())){
-                System.out.println("Rock");
-                url = "https://mp3.ffh.de/ffhchannels/hqrock.mp3";
-            }
-            if("The 80's".equals(e.getActionCommand())){
-                System.out.println("The 80's");
-                url = "https://mp3.ffh.de/ffhchannels/hq80er.mp3";
-            }
-            if("Pur Deutsch".equals(e.getActionCommand())){
-                System.out.println("Pur Deutsch");
-                url = "https://mp3.ffh.de/ffhchannels/hqdeutsch.mp3";
+
+            String url = null;
+
+            //If there is a row selected on the table, sets url to that element.
+            if (table.getSelectedRow() != -1) {
+                url = (String)table.getValueAt(currentSelectedRow, 0);
             }
 
-
+            //Attempts to play the url.
             try {
-                nowPlaying.setText(url);
                 player.open(new URL(url));
                 player.play();
             } catch (MalformedURLException ex) {
@@ -78,7 +95,5 @@ public class StreamPlayerGUI extends JFrame {
                 Logger.getLogger(StreamPlayerGUI.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-
-
     }
 }
